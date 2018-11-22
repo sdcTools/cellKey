8a2fd4738e17eb184e908fe0e7bf3e2f10f49c55
Bernhard Meindl <bernhard.meindl@gmail.com>
2018-11-22 12:36:09 +0100

    delete jenkins

--- scripts-jenkins/test.R
+++
@@ -1,9 +0,0 @@
-library(devtools)
-if (dir.exists("tests") || dir.exists("test")) {
-  message("Tests starten!")
-  testOutput <- devtools::test()
-  #TODO: testOutput weiter verarbeiten ?!?!
-  message("Tests beendet!")
-} else {
-  message("Keine Tests definiert!")
-}
