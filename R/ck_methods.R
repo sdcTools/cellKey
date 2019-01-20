a7ca436fdf46ff65f361062226ad6be96c3aa79e
Bernhard Meindl <bernhard.meindl@gmail.com>
2019-01-20 10:37:39 +0000

    continue ci

--- R/ck_methods.R
+++ R/ck_methods.R
@@ -5,14 +5,14 @@
 #' @param vname if not \code{NULL}, a character specifying a tabulated count
 #' or numeric variable for which for all combinations of dimensional variables the
 #' (perturbed) unweighted and (perturbed) weighted counts are printed.
-setMethod(f="print", signature="pert_table",
-definition=function(x, vname=NULL) {
+setMethod(f = "print", signature = "pert_table",
+definition = function(x, vname=NULL) {
   if (!is.null(vname)) {
     stopifnot(is_scalar_character(vname))
-    validVars <- c(slot(x, "countVars"), slot(x,"numVars"))
+    validVars <- c(slot(x, "countVars"), slot(x, "numVars"))
     if (!vname %in% validVars) {
-      txt <- paste("specified variable name", shQuote(vname),"is invalid.\n")
-      txt <- paste0(txt, "It should be one of ", paste(shQuote(validVars), collapse=", "),".")
+      txt <- paste("specified variable name", shQuote(vname), "is invalid.\n")
+      txt <- paste0(txt, "It should be one of ", paste(shQuote(validVars), collapse = ", "), ".")
       stop(txt)
     }
   }
@@ -23,45 +23,45 @@ definition=function(x, vname=NULL) {
   } else {
     txt <- paste(txt, "unweighted")
   }
-  txt <- paste0(txt, " ",length(slot(x, "dimVars")),"-dimensional table")
+  txt <- paste0(txt, " ", length(slot(x, "dimVars")), "-dimensional table")
   txt <- paste0(txt, " consists of ", nrow(slot(x, "tab")), " cells.")
 
-  if (slot(x, "by")=="Total") {
+  if (slot(x, "by") == "Total") {
     txt <- paste(txt, "The results are based on all units in the input data.")
   } else {
     txt <- paste(txt, "The results are restricted to units for which by-variable")
-    txt <- paste(txt, shQuote(slot(x, "by")),"equals 1.")
+    txt <- paste(txt, shQuote(slot(x, "by")), "equals 1.")
   }
   cat(txt, "\n")
 
   txt <- "The dimensions are given by the following variables\no"
-  txt <- paste(txt, paste(slot(x, "dimVars"), collapse="\no "))
-  cat(txt,"\n\n")
+  txt <- paste(txt, paste(slot(x, "dimVars"), collapse = "\no "))
+  cat(txt, "\n\n")
 
   txt <- paste("Type of pTable-used:", shQuote(slot(x, "type")))
-  cat(txt,"\n")
+  cat(txt, "\n")
 
   txt <- "The following count-variables have been tabulated/perturbed:\no"
-  txt <- paste(txt, paste(slot(x, "countVars"), collapse="\no "))
-  cat(txt,"\n")
+  txt <- paste(txt, paste(slot(x, "countVars"), collapse = "\no "))
+  cat(txt, "\n")
 
-  if (length(slot(x, "numVars"))==0) {
+  if (length(slot(x, "numVars")) == 0) {
     txt <- "No numeric variables have been tabulated/perturbed in this table"
   } else {
     txt <- "The following numeric variables have been tabulated/perturbed:\no"
-    txt <- paste(txt, paste(slot(x, "numVars"), collapse="\no "))
+    txt <- paste(txt, paste(slot(x, "numVars"), collapse = "\no "))
   }
-  cat(txt,"\n")
+  cat(txt, "\n")
 
   if (!is.null(vname)) {
     dt <- slot(x, "tab")
 
     if (vname %in% slot(x, "countVars")) {
-      vnames <- c(slot(x, "dimVars"), paste0(c("UWC","WC","pUWC","pWC"),"_", vname))
+      vnames <- c(slot(x, "dimVars"), paste0(c("UWC", "WC", "pUWC", "pWC"), "_", vname))
     } else {
-      vnames <- c(slot(x, "dimVars"), paste0(c("UW","WS","pUW","pWS"),"_", vname))
+      vnames <- c(slot(x, "dimVars"), paste0(c("UW", "WS", "pUW", "pWS"), "_", vname))
     }
-    dt <- dt[,vnames, with=F]
+    dt <- dt[, vnames, with = FALSE]
     cat("Total-Counts\n")
     print(dt)
   }
@@ -72,44 +72,40 @@ definition=function(x, vname=NULL) {
 #' @exportMethod summary
 #' @rdname ck_methods
 #' @param object input object of class \code{\linkS4class{pert_table}}
-setMethod(f="summary", signature="pert_table",
-definition=function(object) {
+setMethod(f = "summary", signature = "pert_table",
+definition = function(object) {
   vals.pert <- pert <- NULL
   cat("Perturbation statistics count variables:\n")
   info_cnts <- mod_counts(object)
 
-  cnt_info <- info_cnts[,as.list(get_distr_vals(pert)), by="countVar"]
+  cnt_info <- info_cnts[, as.list(get_distr_vals(pert)), by = "countVar"]
   print(cnt_info)
   cat("\n")
 
   # unweighted only!
   cnt_measures <- lapply(slot(object, "countVars"), function(x) {
-    ck_cnt_measures(object, vname=x)
+    ck_cnt_measures(object, vname = x)
   })
   names(cnt_measures) <- slot(object, "countVars")
 
   for (vv in names(cnt_measures)) {
-    cat("Distance-Based measures for count-variable", shQuote(vv),"\n")
+    cat("Distance-Based measures for count-variable", shQuote(vv), "\n")
     print(cnt_measures[[vv]]$measures)
     cat("\n")
-    #cat("Empirical cummulative distribution for perturbation of", shQuote(vv),"\n")
-    #print(cnt_measures[[vv]]$cumdistrA)
-    #print(cnt_measures[[vv]]$cumdistrB)
-
   }
 
   info_nums <- mod_numvars(object)
   num_info <- num_ratios <- NULL
-  if (nrow(info_nums)>0) {
-    num_info <- info_nums[,as.list(get_distr_vals(vals.pert)), by="numVar"]
+  if (nrow(info_nums) > 0) {
+    num_info <- info_nums[, as.list(get_distr_vals(vals.pert)), by = "numVar"]
     cat("\nPerturbation statistics on numerical variables:\n")
     print(num_info)
   }
 
   return(invisible(
-    list(cnt_info=cnt_info,
-    cnt_measures=cnt_measures,
-    num_info=num_info)
+    list(cnt_info = cnt_info,
+    cnt_measures = cnt_measures,
+    num_info = num_info)
   ))
 })
 
@@ -127,15 +123,15 @@ setGeneric("mod_counts", function(x, ...) {
 
 #' @rdname mod_counts-methods
 #' @param verbose (logical) if \code{TRUE}, additional information is printed
-setMethod(f="mod_counts", signature="pert_table",
-definition=function(x, verbose=FALSE) {
+setMethod(f = "mod_counts", signature = "pert_table",
+definition = function(x, verbose=FALSE) {
   type <- slot(x, "type")
   stopifnot(is_scalar_logical(verbose))
   if (verbose) {
     message("The perturbation was based on the ", shQuote(type), " method.")
   }
   cc <- slot(x, "count_modifications")
-  if (type=="destatis") {
+  if (type == "destatis") {
     cc$col_indices <- NULL
   }
   cc
@@ -151,8 +147,8 @@ setGeneric("mod_numvars", function(x, ...) {
   standardGeneric("mod_numvars")
 })
 #' @rdname mod_numvars-methods
-setMethod(f="mod_numvars", signature="pert_table",
-definition=function(x) {
+setMethod(f = "mod_numvars", signature = "pert_table",
+definition = function(x) {
   slot(x, "numvars_modifications")
 })
 NULL
